99df3818c05ed4a89de13efb0c6870d24d8af437
Aparus <rustamapaev@gmail.com>
2018-10-18 17:45:04 +0300

    17 - Connecting Redux to Firebase установили и настроили плагины react-redux-firebase и redux-firestore, которые позволяют подключать облачное хранилище к store и синхронизировать их

--- src/index.js
+++ src/index.js
@@ -3,15 +3,29 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import rootReducer from './store/reducers/rootReducer'
 import { Provider } from 'react-redux'
 // <Provider> делает store доступным из приложения App.
 import thunk from 'redux-thunk'
-const store = createStore(rootReducer, applyMiddleware(thunk))
+import { reduxFirestore, getFirestore } from 'redux-firestore'
+import { reactReduxFirebase, getFirebase } from 'react-redux-firebase'
+// getFirebase и getFirestore обеспечивают синхронизацию store нашего приложения с облачным firebase & filestore
+import fbConfig from './config/firebase'
+
+const store = createStore(
+    rootReducer,
+    compose(
+        applyMiddleware(thunk.withExtraArgument({ getFirestore, getFirebase })),
+        reduxFirestore(fbConfig),
+        reactReduxFirebase(fbConfig)
+    )
+)
 // middleware добавляет функционал для store,
 // thunk в данном случае добавляет возможность в экшенах выполнять асинхронные запросы
 // если бы мы не добавили эту мидлвару, то экшены выстреливались бы моментально, не дожидаясь асинхр.ответов
+// подобно тому, как мы использовали compose для соединения reducers,
+// теперь мы используем её же для соединения дополнительных функционалов, подключаемых к store
 
 ReactDOM.render(
     <Provider store={store}>
