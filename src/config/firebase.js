23696bc159b30bf88fba986fb61a05fcc0fe04d5
Aparus <rustamapaev@gmail.com>
2018-10-18 14:37:07 +0300

    15 - Creating a Firebase Project установили firebase через npm, создали новый проект на firebase.google.com, подключили проект к нашему приложению. экспортируем из модуля firebase - это и будет наш бэкенд.

---
+++ src/config/firebase.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+// Initialize Firebase
+var config = {
+    apiKey: 'AIzaSyD-ZnOjxzDdgMEJKOVvHpPLUQWQxTIpQ3I',
+    authDomain: 'marioplan-58907.firebaseapp.com',
+    databaseURL: 'https://marioplan-58907.firebaseio.com',
+    projectId: 'marioplan-58907',
+    storageBucket: 'marioplan-58907.appspot.com',
+    messagingSenderId: '763989227498',
+}
+
+firebase.initializeApp(config)
+firebase.firestore().settings({ timestampsInSnapshots: true })
+
+export default firebase
