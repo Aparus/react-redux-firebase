618dfca733c1b5c9c47f747dc78204938fcec04f
Aparus <rustamapaev@gmail.com>
2018-10-19 19:06:59 +0300

    23 - Logging Users In 1) создали экшн signIn в authActions. он делает запрос в облако и в зависимости от ответа выстреливает экшены Login_Success OR Login_Error 2) в authReducer добавили обработку этих экшенов. при ошибке авторизации в state появляется новая величина authError 3) компонент SignIn подключили к store. Теперь при нажатии на кнопку Login выстреливается экшн и попадает в диспетчер

--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -1,5 +1,7 @@
 // SignIn , SignUp - оба классовые компоненты, потому что нам нужно отслеживать ввод пользователя
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { signIn } from '../../store/actions/authActions'
 
 class SignIn extends Component {
     state = {
@@ -11,9 +13,10 @@ class SignIn extends Component {
     }
     handleSubmit = e => {
         e.preventDefault()
-        console.log(this.state)
+        this.props.signIn(this.state)
     }
     render() {
+        const { authError } = this.props
         return (
             <div className="container">
                 <form onSubmit={this.handleSubmit} className="white">
@@ -38,6 +41,9 @@ class SignIn extends Component {
                         <button className="btn pink lighten-1 z-depth-0">
                             Login
                         </button>
+                        <div className="red-text center">
+                            {authError ? <p>{authError}</p> : null}
+                        </div>
                     </div>
                 </form>
             </div>
@@ -45,4 +51,19 @@ class SignIn extends Component {
     }
 }
 
-export default SignIn
+const mapStateToProps = state => {
+    return {
+        authError: state.auth.authError,
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        signIn: creds => dispatch(signIn(creds)),
+    }
+}
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(SignIn)
